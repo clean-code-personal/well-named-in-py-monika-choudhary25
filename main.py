fa71fa52a5810e4fbbb068e2a52a7cab6424b572
monika-choudhary25 <monika.choudhary@philips.com>
2024-10-29 10:19:41 +0530

    Updating main to call print feature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from color_constants import MAJOR_COLORS, MINOR_COLORS
 from color_mapping import get_color_from_pair_number, get_pair_number_from_color
 from test_color_mapping import test_pair_to_number,test_number_to_pair
+from reference_manual import print_reference_manual
 
 if __name__ == '__main__':
   test_number_to_pair(4, 'White', 'Brown')
@@ -8,4 +9,7 @@ if __name__ == '__main__':
   test_pair_to_number('Black', 'Orange', 12)
   test_pair_to_number('Violet', 'Slate', 25)
   test_pair_to_number('Red', 'Orange', 7)
+
+  print("Color Code Reference Manual:")
+  print(print_reference_manual())
   print('Done :)')
