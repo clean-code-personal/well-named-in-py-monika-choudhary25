9a203ba60543fde382fe839c2dcc2d563a91f656
monika-choudhary25 <monika.choudhary@philips.com>
2024-10-29 10:16:55 +0530

    Adding printing reference manual feature

--- reference_manual.py
+++ reference_manual.py
@@ -1,2 +1,11 @@
+from color_mapping import get_color_from_pair_number
+
 def color_pair_to_string(major_color, minor_color):
   return f'{major_color} {minor_color}' 
+
+def print_reference_manual():
+    manual = []
+    for pair_number in range(1, 26):
+        major_color, minor_color = get_color_from_pair_number(pair_number)
+        manual.append(f'{pair_number}: {color_pair_to_string(major_color, minor_color)}')
+    return '\n'.join(manual)
